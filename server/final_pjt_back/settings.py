e02b6a485d57ae21f81083794f42c90b1583c554
unknown <swqq31@gmail.com>
2022-12-18 14:21:09 +0900

    settings

--- server/final_pjt_back/settings.py
+++ server/final_pjt_back/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-rt%yu%_9^$c3sa7$l+r-m_ax$t7f@1&(d-5&i70=e)l%ha@33r'
+SECRET_KEY = '************************************************************'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
